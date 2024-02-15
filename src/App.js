f4eedfd2981def5b868cfb3b1e2ad72df4082fc9
Thilina Dilshan <tilinadilshan1999@gmail.com>
2024-02-15 21:43:55 +0530

    complete final jsx

--- src/App.js
+++ src/App.js
@@ -1,14 +1,13 @@
 import './App.css';
-import Final from './pages/Final/Final';
 //import Countdown from './pages/CountDown/Countdown';
-//import Home from './pages/Home/Home';
+import Home from './pages/Home/Home';
 import ParticleEffect from './pages/ParticlesEffect/ParticleEffect';
 
 function App() {
   return (
     <div className="App">
       <ParticleEffect/>
-      <Final/>
+      <Home/>
     </div>
   );
 }
--- src/pages/Final/Final.jsx
+++ src/pages/Final/Final.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import './Final.css';
 import ParticleEffect from '../ParticlesEffect/ParticleEffect';
-import item1Img from '../../assets/item1.png';
 
 export default function Final() {
   return (
@@ -10,11 +9,14 @@ export default function Final() {
         <div className="title">
           <h1>Congratulations</h1>
         </div>
+        <div className="subTitle">
+          <p>Now you have access to the heart</p>
+        </div>
         <div className="wrapper">
         <div className="itemContainer">
-            <div className="item1"></div>
-            <div className="item2"></div>
-            <div className="item3"></div>
+            <div id="item1" className='item' ></div>
+            <div id="item2" className='item' ></div>
+            <div id="item3" className='item' ></div>
         </div>
         </div>
     </div>
