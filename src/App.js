8ca33149d6c1fdd4b6c0297aa00667befe2713aa
Thilina Dilshan <tilinadilshan1999@gmail.com>
2024-02-17 18:59:19 +0530

    import Final page

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+//import Final from './pages/Final/Final';
 //import Countdown from './pages/CountDown/Countdown';
 import Home from './pages/Home/Home';
 import ParticleEffect from './pages/ParticlesEffect/ParticleEffect';
